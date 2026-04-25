f21c641c6c202461de0c211e2a4dfb65c5af5e59
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-04-24 22:31:48 -0500

    Fix rendering

--- src/main/java/mod/emt/kami/client/model/armor/ModelIchorArmor.java
+++ src/main/java/mod/emt/kami/client/model/armor/ModelIchorArmor.java
@@ -1,45 +1,30 @@
 package mod.emt.kami.client.model.armor;
 
-import mod.emt.kami.Kami;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.math.MathHelper;
 import org.jetbrains.annotations.NotNull;
+import org.lwjgl.opengl.GL11;
 
 public class ModelIchorArmor extends ModelCustomArmor {
-    protected static final String TEXTURE_PATH_DYED_OVERLAY_1 = new ResourceLocation(Kami.MOD_ID, "textures/models/armor/ichorweave_layer_1_dyed_overlay.png").toString();
-    protected static final String TEXTURE_PATH_DYED_OVERLAY_2 = new ResourceLocation(Kami.MOD_ID, "textures/models/armor/ichorweave_layer_2_dyed_overlay.png").toString();
-
     public ModelIchorArmor(float modelSize) {
         super(modelSize, 64, 32);
     }
 
     @Override
     public void render(@NotNull Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch, float scale) {
-        super.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
+        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240f, 240f);
 
-        GlStateManager.pushMatrix();
-        GlStateManager.enableBlend();
-        GlStateManager.disableLighting();
-        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240.0F, 240.0F);
-
-        float pulse = (MathHelper.sin(ageInTicks * 0.1F) + 1.0F) / 2.0F;
-        float red = 1.0F;
-        float green = 0.8F + (pulse * 0.2F);
-        float blue = 0.1F + (pulse * 0.9F);
-
-        GlStateManager.color(red, green, blue, 1.0F);
-        Minecraft.getMinecraft().getTextureManager().bindTexture(new ResourceLocation(this.bipedLeftLeg.showModel ? TEXTURE_PATH_DYED_OVERLAY_2 : TEXTURE_PATH_DYED_OVERLAY_1));
+        if (GL11.glIsEnabled(GL11.GL_LIGHTING)) {
+            GlStateManager.disableLighting();
+        }
 
         super.render(entity, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch, scale);
 
-        GlStateManager.enableLighting();
-        GlStateManager.disableBlend();
+        if (GL11.glIsEnabled(GL11.GL_LIGHTING)) {
+            GlStateManager.enableLighting();
+        }
+
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, OpenGlHelper.lastBrightnessX, OpenGlHelper.lastBrightnessY);
-        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
-        GlStateManager.popMatrix();
     }
 }
