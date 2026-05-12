cd4dd940b4ef9f8f7683c3138f1827267f8be654
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-05-12 04:49:25 -0500

    Add molten ichorium for TiC

--- src/main/java/mod/emt/kami/compat/KamiCompatHandler.java
+++ src/main/java/mod/emt/kami/compat/KamiCompatHandler.java
@@ -3,8 +3,11 @@ package mod.emt.kami.compat;
 import mod.emt.kami.Kami;
 import mod.emt.kami.compat.tinkers.ConstructsArmory;
 import mod.emt.kami.compat.tinkers.TinkersConstruct;
+import mod.emt.kami.compat.tinkers.TinkersConstructClient;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.relauncher.FMLLaunchHandler;
 
 // TODO: Add config options to checks
 @Mod.EventBusSubscriber(modid = Kami.MOD_ID)
@@ -14,6 +17,9 @@ public class KamiCompatHandler {
         if (Loader.isModLoaded("tconstruct"))
         {
             TinkersConstruct.preInit();
+            if (FMLLaunchHandler.side().isClient()) {
+                MinecraftForge.EVENT_BUS.register(new TinkersConstructClient());
+            }
 
             // Only load Construct's Armory if Tinkers' Construct is also loaded
             if (Loader.isModLoaded("conarm"))
@@ -34,6 +40,14 @@ public class KamiCompatHandler {
             {
                 ConstructsArmory.init();
             }
+        }
+    }
+
+    public static void postInit()
+    {
+        if (Loader.isModLoaded("tconstruct"))
+        {
+            TinkersConstruct.postInit();
         }
     }
 }
--- src/main/java/mod/emt/kami/compat/tinkers/TinkersConstruct.java
+++ src/main/java/mod/emt/kami/compat/tinkers/TinkersConstruct.java
@@ -4,23 +4,42 @@ import mod.emt.kami.Kami;
 import mod.emt.kami.compat.tinkers.modifiers.ModDivineMandate;
 import mod.emt.kami.compat.tinkers.traits.TraitGodComplex;
 import mod.emt.kami.registry.ModItemsKAMI;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
 import slimeknights.mantle.client.book.repository.ModuleFileRepository;
 import slimeknights.tconstruct.library.TinkerRegistry;
 import slimeknights.tconstruct.library.book.TinkerBook;
+import slimeknights.tconstruct.library.fluid.FluidMolten;
 import slimeknights.tconstruct.library.materials.*;
 import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
+import slimeknights.tconstruct.smeltery.TinkerSmeltery;
+import slimeknights.tconstruct.smeltery.block.BlockMolten;
 
 public class TinkersConstruct {
     // These materials are used universally between tools and armor
     public static final Material ICHORIUM = new Material("kami_ichorium", 0xFF910C);
+
+    // Fluids
+    public static final FluidMolten ICHORIUM_FLUID = new FluidMolten("ichorium", 0xD67B09);
 
     // Normally you'd need both tool and armor traits, but this one works with both just fine
     public static final AbstractTrait GOD_COMPLEX = new TraitGodComplex();
 
     // Modifiers
     public static Modifier modDivineMandate;
+
+    // TODO: Return bucket when picked from JEI
+    public static void registerFluid(Fluid fluid) {
+        FluidRegistry.addBucketForFluid(fluid);
+        BlockMolten blockMolten = (BlockMolten) new BlockMolten(fluid).setRegistryName(Kami.MOD_ID, "molten_" + fluid.getName());
+        ItemBlock itemBlockMolten = (ItemBlock) new ItemBlock(blockMolten).setRegistryName(blockMolten.getRegistryName());
+        ForgeRegistries.BLOCKS.register(blockMolten);
+        ForgeRegistries.ITEMS.register(itemBlockMolten);
+    }
 
     public static void preInit()
     {
@@ -30,7 +49,9 @@ public class TinkersConstruct {
                 new ExtraMaterialStats(100),
                 new BowMaterialStats(1.2F, 1.0F, 8.0F));
         ICHORIUM.addTrait(GOD_COMPLEX);
-        TinkerRegistry.integrate(ICHORIUM, null, "Ichorium").preInit();
+        registerFluid(ICHORIUM_FLUID);
+        ICHORIUM_FLUID.setTemperature(580);
+        TinkerRegistry.integrate(ICHORIUM, ICHORIUM_FLUID, "Ichorium").preInit();
 
         modDivineMandate = new ModDivineMandate();
     }
@@ -42,8 +63,16 @@ public class TinkersConstruct {
 
         ICHORIUM.addCommonItems("Ichorium");
         ICHORIUM.setRepresentativeItem(ModItemsKAMI.ICHORIUM_INGOT);
-        ICHORIUM.setCraftable(true).setCastable(false);
+        ICHORIUM.setFluid(ICHORIUM_FLUID);
+        ICHORIUM.setCraftable(false).setCastable(true);
 
         modDivineMandate.addItem(ModItemsKAMI.ICHORIUM_INGOT, 1, 1);
+    }
+
+    public static void postInit()
+    {
+        // Smeltery stuff goes here
+        TinkerSmeltery.registerToolpartMeltingCasting(ICHORIUM);
+        // TODO: Check and see if ingots and blocks also need to be registered here or if it's already handled
     }
 }
