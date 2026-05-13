a32ba7b82f74dda50453bc28d61651852431bad8
Invadermonky <101663980+Invadermonky@users.noreply.github.com>
2026-05-13 12:43:29 -0700

    add block registration

--- src/main/java/mod/emt/kami/registry/RegistrarKAMI.java
+++ src/main/java/mod/emt/kami/registry/RegistrarKAMI.java
@@ -12,6 +12,7 @@ import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.entity.Entity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
@@ -57,12 +58,21 @@ public class RegistrarKAMI {
     public static void registerItems(RegistryEvent.Register<Item> event) {
         IForgeRegistry<Item> registry = event.getRegistry();
         ModItemsKAMI.initItems();
+        ModBlocksKAMI.MOD_BLOCKS.forEach(block -> registry.register(new ItemBlock(block)
+                        .setRegistryName(Objects.requireNonNull(block.getRegistryName()))
+                        .setTranslationKey(block.getTranslationKey())
+                        .setCreativeTab(block.getCreativeTab())
+        ));
         ModItemsKAMI.MOD_ITEMS.forEach(registry::register);
     }
 
     @SideOnly(Side.CLIENT)
     @SubscribeEvent
     public static void registerModels(ModelRegistryEvent event) {
+        ModBlocksKAMI.MOD_BLOCKS.forEach(block -> {
+            ModelResourceLocation loc = new ModelResourceLocation(Objects.requireNonNull(block.getRegistryName()), "inventory");
+            ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, loc);
+        });
         ModItemsKAMI.MOD_ITEMS.forEach(item -> {
             ModelResourceLocation loc = new ModelResourceLocation(Objects.requireNonNull(item.getRegistryName()), "inventory");
             ModelLoader.setCustomModelResourceLocation(item, 0, loc);
@@ -72,8 +82,6 @@ public class RegistrarKAMI {
     @SubscribeEvent
     public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {
         ModRecipesKAMI.initRecipes(event);
-        //IForgeRegistry<IRecipe> registry = event.getRegistry();
-        //getAdditions().forEach(addition -> addition.registerRecipe(registry));
         event.getRegistry().register(new DyeableItemRecipe().setRegistryName(new ResourceLocation(Kami.MOD_ID, "dyeable_item")));
     }
 
