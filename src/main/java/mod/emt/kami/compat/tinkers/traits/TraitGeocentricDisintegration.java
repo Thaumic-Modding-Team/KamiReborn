8d775b577a5059666e5b3e602cf19e40cd9706ae
IcarussOne <135455255+IcarussOne@users.noreply.github.com>
2026-06-19 08:25:14 -0500

    Add appropriate check

--- src/main/java/mod/emt/kami/compat/tinkers/traits/TraitGeocentricDisintegration.java
+++ src/main/java/mod/emt/kami/compat/tinkers/traits/TraitGeocentricDisintegration.java
@@ -1,9 +1,12 @@
 package mod.emt.kami.compat.tinkers.traits;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumHand;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import slimeknights.tconstruct.library.modifiers.IToolMod;
+import slimeknights.tconstruct.library.tinkering.Category;
 import slimeknights.tconstruct.library.traits.AbstractTrait;
+import slimeknights.tconstruct.library.utils.ToolHelper;
 import slimeknights.tconstruct.tools.TinkerModifiers;
 import slimeknights.tconstruct.tools.TinkerTraits;
 
@@ -23,6 +26,9 @@ public class TraitGeocentricDisintegration extends AbstractTrait {
 
     @Override
     public void miningSpeed(ItemStack tool, PlayerEvent.BreakSpeed event) {
-        event.setNewSpeed(event.getOriginalSpeed() * 1000f);
+        ItemStack stack = event.getEntityPlayer().getHeldItem(EnumHand.MAIN_HAND);
+        if (ToolHelper.hasCategory(stack, Category.HARVEST)) {
+            event.setNewSpeed(event.getOriginalSpeed() * 1000);
+        }
     }
 }
